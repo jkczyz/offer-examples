a79438dfcdd5e62b343fd77c8f0e25fe005388aa
Jeffrey Czyz <jkczyz@gmail.com>
2023-06-30 16:18:37 -0400

    Print offer size and use local dependency

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,81 @@
 use bitcoin::secp256k1::{PublicKey, Secp256k1, SecretKey};
-use lightning::chain::keysinterface::EntropySource;
+use lightning::sign::EntropySource;
 use lightning::offers::offer::OfferBuilder;
-use lightning::onion_message::BlindedPath;
+use lightning::blinded_path::BlindedPath;
 
 fn main() {
 	// Zero-amount, empty description
 
 	let offer = OfferBuilder::new("".to_string(), pubkey(42)).build().unwrap();
-	println!("Zero-amount offer with empty description:\n{}\n", offer);
+	println!("Zero-amount offer with empty description (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	let offer = OfferBuilder::new("".to_string(), pubkey(42))
 		.path(blinded_path(&vec![pubkey(44), pubkey(43)]))
 		.build().unwrap();
-	println!("Zero-amount offer with empty description and one blinded path:\n{}\n", offer);
+	println!("Zero-amount offer with empty description and one blinded path (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	let offer = OfferBuilder::new("".to_string(), pubkey(42))
 		.path(blinded_path(&vec![pubkey(44), pubkey(43)]))
 		.path(blinded_path(&vec![pubkey(45), pubkey(43)]))
 		.build().unwrap();
-	println!("Zero-amount offer with empty description and two blinded paths:\n{}\n", offer);
+	println!("Zero-amount offer with empty description and two blinded paths (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	// Zero-amount, description
 
 	let offer = OfferBuilder::new("Pay Esmeralda Thompson".to_string(), pubkey(42))
 		.build().unwrap();
-	println!("Zero-amount offer with description:\n{}\n", offer);
+	println!("Zero-amount offer with description (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	let offer = OfferBuilder::new("Pay Esmeralda Thompson".to_string(), pubkey(42))
 		.path(blinded_path(&vec![pubkey(44), pubkey(43)]))
 		.build().unwrap();
-	println!("Zero-amount offer with description and one blinded path:\n{}\n", offer);
+	println!("Zero-amount offer with description and one blinded path (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	let offer = OfferBuilder::new("Pay Esmeralda Thompson".to_string(), pubkey(42))
 		.path(blinded_path(&vec![pubkey(44), pubkey(43)]))
 		.path(blinded_path(&vec![pubkey(45), pubkey(43)]))
 		.build().unwrap();
-	println!("Zero-amount offer with description and two blinded paths:\n{}\n", offer);
+	println!("Zero-amount offer with description and two blinded paths (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	// Amount, empty description
 
 	let offer = OfferBuilder::new("".to_string(), pubkey(42))
 		.amount_msats(2_000_000)
 		.build().unwrap();
-	println!("Offer with empty description:\n{}\n", offer);
+	println!("Offer with empty description (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	let offer = OfferBuilder::new("".to_string(), pubkey(42))
 		.amount_msats(2_000_000)
 		.path(blinded_path(&vec![pubkey(44), pubkey(43)]))
 		.build().unwrap();
-	println!("Offer with empty description and one blinded path:\n{}\n", offer);
+	println!("Offer with empty description and one blinded path (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	let offer = OfferBuilder::new("".to_string(), pubkey(42))
 		.amount_msats(2_000_000)
 		.path(blinded_path(&vec![pubkey(44), pubkey(43)]))
 		.path(blinded_path(&vec![pubkey(45), pubkey(43)]))
 		.build().unwrap();
-	println!("Offer with empty description and two blinded paths:\n{}\n", offer);
+	println!("Offer with empty description and two blinded paths (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	// Amount, description
 
 	let offer = OfferBuilder::new("Pay Esmeralda Thompson".to_string(), pubkey(42))
 		.amount_msats(2_000_000)
 		.build().unwrap();
-	println!("Offer with description:\n{}\n", offer);
+	println!("Offer with description (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	let offer = OfferBuilder::new("Pay Esmeralda Thompson".to_string(), pubkey(42))
 		.amount_msats(2_000_000)
 		.path(blinded_path(&vec![pubkey(44), pubkey(43)]))
 		.build().unwrap();
-	println!("Offer with description and one blinded path:\n{}\n", offer);
+	println!("Offer with description and one blinded path (len: {}):\n{}\n", offer.to_string().len(), offer);
 
 	let offer = OfferBuilder::new("Pay Esmeralda Thompson".to_string(), pubkey(42))
 		.amount_msats(2_000_000)
 		.path(blinded_path(&vec![pubkey(44), pubkey(43)]))
 		.path(blinded_path(&vec![pubkey(45), pubkey(43)]))
 		.build().unwrap();
-	println!("Offer with description and two blinded paths:\n{}\n", offer);
+	println!("Offer with description and two blinded paths (len: {}):\n{}\n", offer.to_string().len(), offer);
 }
 
 struct Randomness;
@@ -98,5 +98,5 @@ fn privkey(byte: u8) -> SecretKey {
 fn blinded_path(hops: &[PublicKey]) -> BlindedPath {
 	let secp_ctx = Secp256k1::new();
 	let randomness = Randomness {};
-	BlindedPath::new(hops, &randomness, &secp_ctx).unwrap()
+	BlindedPath::new_for_message(hops, &randomness, &secp_ctx).unwrap()
 }
